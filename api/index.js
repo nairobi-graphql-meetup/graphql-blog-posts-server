e5eb0ac4450ac35fd8d3ecc78045b50ccf914039
Joe Ng'ethe <>
2019-05-02 14:30:24 +0300

    added @todo comments

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@
  * @todo
  * - Build a graphql server
  * - define our typeDefs and resolvers
- * - create an API to READ, DELETE, UPDATE and PUT data
+ * - create an API to READ, DELETE, UPDATE and PUT data in memory; data won'r be persistent
  * - We'll assume we are building an API for a blog post application
  * */
